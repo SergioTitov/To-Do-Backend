10276fb8fc73e001cef65736c4cd67977ded56d9
SergioTitov <sergejtitov848@gmail.com>
2022-12-09 16:53:52 +0300

    2

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // import express from "express";
-console.log('gooo');
+// console.log("gooo");
+// let b = 5;
+
+// function t() {
+//   let a = 7;
+//   let c = a + b;
+//   b= a+d
+//   console.log(a + c);
+// }
+// t();
+const arg = process.argv
+console.log(arg[3]);
